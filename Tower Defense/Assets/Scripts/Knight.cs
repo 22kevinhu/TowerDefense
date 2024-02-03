6b7f0b54bc32c3a2d811827d0a869f5462994268
Kevin Hu <22kevinhu@gmail.com>
2024-02-03 15:00:51 -0500

    updated scripts

--- Tower Defense/Assets/Scripts/Knight.cs
+++ Tower Defense/Assets/Scripts/Knight.cs
@@ -6,6 +6,9 @@ public class Knight : MonoBehaviour
 {
     public int health;
     public float speed;
+    public Rigidbody2D knightRb;
+    public BoxCollider2D knightBc;
+    public BoxCollider2D leftTowerBc;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +18,16 @@ public class Knight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        while (health != 0) {
-            transform.position += Time.deltaTime * Vector3.left * speed;
+        while (health != 0)
+        {
+            knightRb.velocity += Time.deltaTime * Vector2.left * speed;
+            health--;
         }
 
+    }
+
+    private void OnCollisionEnter2d(Collision collision)
+    {
+        health = 0;
     }
 }
